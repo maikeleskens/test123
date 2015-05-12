939027ef488edd41769d6dff171bd4c539f05dc6
maikeleskens <maikeleskens@gmail.com>
2015-05-12 11:13:42 +0200

    First test
    This is the first test

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+	app = express(),
+	server = require ('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+server.listen(1337, '0.0.0.0');
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+//reveive server side
+
+//first thing that happens when a client connects to a socket.io application
+//turn on connection event, uses socket that user uses
+io.sockets.on('connection', function(socket){
+	//look for the name of the event you emit on client side and get the data:
+	socket.on('send message', function(data){
+		//emit a new event on socket, and send the data from the function above. sends to all the users
+		io.sockets.emit('new message', data);
+	});
+});
